d32fd77f32d65d1f390e54df8789aafadae1767d
juansara <68070135+juansara@users.noreply.github.com>
2021-11-02 20:05:02 -0500

    Add files via upload

--- Graph/graph.h
+++ Graph/graph.h
@@ -20,34 +20,33 @@ class Graph;
 
 template<typename TV, typename TE>
 struct Edge {
-    Vertex<TV, TE>* vertexes[2];
-    TE weight;
+    Vertex<TV, TE>* vertexes[2]; // un array de vertices donde almacena 2 nodos que estan conectados
+    TE weight; //peso de la arista
 };
 
 template<typename TV, typename TE>
 struct Vertex {
-    TV data;
-    std::list<Edge<TV, TE>*> edges;
+    TV data; //Data del vertice
+    string id;
+    std::list<Edge<TV, TE>*> edges; //lista de aristas a las cuales esta conectada el nodo
 };
 
 template<typename TV, typename TE>
 class Graph{
 private:    
-    std::unordered_map<string, Vertex<TV, TE>*>  vertexes;
-    
+    std::unordered_map<string, Vertex<TV, TE>*>  vertexes; //key = id del nodo, value = vertice
 public:
     virtual bool insertVertex(string id, TV vertex) = 0;   
     virtual bool createEdge(string id1, string id2, TE w) = 0;     
     virtual bool deleteVertex(string id) = 0;     
-    virtual bool deleteEdge(string id) = 0;   
+    virtual bool deleteEdge(string id1, string id2) = 0;   
     virtual TE &operator()(string start, string end)= 0;  
     virtual float density() = 0;
     virtual bool isDense(float threshold = 0.5) = 0;
     virtual bool isConnected()= 0;
     virtual bool isStronglyConnected() throw();
     virtual bool empty();
-    virtual void clear()= 0;  
-      
+    virtual void clear()= 0;
     virtual void displayVertex(string id)= 0;
     virtual bool findById(string id) = 0;
     virtual void display() = 0;
