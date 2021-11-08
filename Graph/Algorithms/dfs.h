f1fb412dd6cf9f5217497df1707363ab98eeafc8
Denis <denis.irala@utec.edu.pe>
2021-11-08 17:00:44 -0500

    Solución Parcial DFS

--- Graph/Algorithms/dfs.h
+++ Graph/Algorithms/dfs.h
@@ -10,53 +10,15 @@ private:
 public:
     dfs() = default;
 
-    dfs(Graph<TV,TE>* &graph, string vertex_id){
-        std::unordered_set<string> visitados;
-        std::stack<pair<string, Vertex<TV,TE>*>> st;
-        string primero;
-
-        auto temp = graph->vertexes
-        auto aux = graph->vertexes[vertex_id];
-        Grafo = new DirectedGraph<TV,TE>;
-        Grafo->insertVertex(vertex_id,axu->data);
-
-        for(auto &i : temp){
-            primero = i.first;
-            visitados.insert(primero);
-            st.push(make_pair(primero,((temp.second)->edges)->vertexes));
-
-            while(!st.empty()){
-                pair<string,Vertex<TV,TE>*> actual = st.top();
-                st.pop();
-                string id;
-                Vertex<TV,TE>* to_insert;
-                id = actual.first;
-                to_insert = actual.second;
-
-                if(visitados.find(to_insert->id) == visitados.end()){
-                    visitados.insert(to_insert->id);
-                    Grafo->insertVertex(to_insert->id,to_insert->data);
-                    Grafo->insertEdge(id,to_insert->id,1)
-                }
-
-                for(auto &a : to_insert->edges){
-                    if(visitados.find(a->vertexes->id) == visitados.end()){
-                        st.push(make_pair(to_insert->id,a->vertexes))
-                    }
-                }
-            }
-        }
-    }
-
     dfs(UnDirectedGraph<TV,TE>* &graph, string vertex_id){
         std::unordered_set<string> visitados;
         std::stack<pair<string, Vertex<TV,TE>*>> st;
         string primero;
 
-        auto temp = graph->vertexes
+        auto temp = graph->vertexes;
         auto aux = graph->vertexes[vertex_id];
         Grafo = new DirectedGraph<TV,TE>;
-        Grafo->insertVertex(vertex_id,axu->data);
+        Grafo->insertVertex(vertex_id,aux->data);
 
         for(auto &i : temp){
             primero = i.first;
@@ -74,12 +36,12 @@ public:
                 if(visitados.find(to_insert->id) == visitados.end()){
                     visitados.insert(to_insert->id);
                     Grafo->insertVertex(to_insert->id,to_insert->data);
-                    Grafo->insertEdge(id,to_insert->id,1)
+                    Grafo->insertEdge(id,to_insert->id,1);
                 }
 
                 for(auto &a : to_insert->edges){
                     if(visitados.find(a->vertexes->id) == visitados.end()){
-                        st.push(make_pair(to_insert->id,a->vertexes))
+                        st.push(make_pair(to_insert->id,a->vertexes));
                     }
                 }
             }
@@ -91,10 +53,10 @@ public:
         std::stack<pair<string, Vertex<TV,TE>*>> st;
         string primero;
 
-        auto temp = graph->vertexes
+        auto temp = graph->vertexes;
         auto aux = graph->vertexes[vertex_id];
         Grafo = new DirectedGraph<TV,TE>;
-        Grafo->insertVertex(vertex_id,axu->data);
+        Grafo->insertVertex(vertex_id,aux->data);
 
         for(auto &i : temp){
             primero = i.first;
@@ -112,12 +74,12 @@ public:
                 if(visitados.find(to_insert->id) == visitados.end()){
                     visitados.insert(to_insert->id);
                     Grafo->insertVertex(to_insert->id,to_insert->data);
-                    Grafo->insertEdge(id,to_insert->id,1)
+                    Grafo->insertEdge(id,to_insert->id,1);
                 }
 
                 for(auto &a : to_insert->edges){
                     if(visitados.find(a->vertexes->id) == visitados.end()){
-                        st.push(make_pair(to_insert->id,a->vertexes))
+                        st.push(make_pair(to_insert->id,a->vertexes));
                     }
                 }
             }
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include "Graph/UndirectedGraph.h"
 #include "Graph/DirectedGraph.h"
+#include "Graph/Algorithms/dfs.h"
 
 using namespace std;
 
@@ -9,17 +10,16 @@ int main(int argc, char *argv[]) {
     std::cout << "================================================" << std::endl;
     std::cout << "MENU GRAPH TESTER" << std::endl;
     std::cout << "================================================" << std::endl;
-    UnDirectedGraph<int, int> GND;
-    GND.insertVertex("A", 5);
-    GND.insertVertex("B", 50);
-    GND.insertVertex("C", 1);
-    GND.insertVertex("D", 0);
-    GND.createEdge("A","B",6);
-    GND.createEdge("B","C",6);
-    GND.createEdge("C","A",2);
-    GND.createEdge("D","B",20);
-
-    cout<<GND.isDense(0.8);
+    UnDirectedGraph<int, int>* GND;
+    GND->insertVertex("A", 5);
+    GND->insertVertex("B", 50);
+    GND->insertVertex("C", 1);
+    GND->insertVertex("D", 0);
+    GND->createEdge("A","B",6);
+    GND->createEdge("B","C",6);
+    GND->createEdge("C","A",2);
+    GND->createEdge("D","B",20);
+    dfs<int,int> X(GND,"A");
 
     return EXIT_SUCCESS;
 }
