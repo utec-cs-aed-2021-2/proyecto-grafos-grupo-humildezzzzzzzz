692043e3282be9e67354d5be218d89d5827bd45a
Denis <denis.irala@utec.edu.pe>
2021-11-29 18:50:01 -0500

    Borrador de menú

--- main.cpp
+++ main.cpp
@@ -8,11 +8,56 @@
 
 using namespace std;
 
+void Mostrar() {
+        std::cout << "1. Insertar Vertice" << endl;
+
+        std::cout << "2. Insertar Arista" << endl;
+
+        std::cout << "3. Ejecutar BFS" << endl;
+
+        std::cout << "4. Ejecutar DFS" << endl;
+
+        std::cout << "5. Ejecutar Kruskal" << endl;
+
+        std::cout << "6. Ejecutar Prim" << endl;
+
+        std::cout << "7. Ejecutar Dijkstra" << endl;
+
+        std::cout << "7. Ejecutar A*" << endl;
+
+        std::cout << "8. Salir" << endl;
+
+        int opcion;
+        std::cout
+            << endl << "Ingrese el número correspondiente:  ";
+        std::cin >> opcion;
+        switch (opcion) {
+            case 1:
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 6:
+                
+                break;
+            default:
+               
+                break;
+        };
+    };
+
+
 int main(int argc, char *argv[]) {
     std::cout << "================================================" << std::endl;
     std::cout << "MENU GRAPH TESTER" << std::endl;
     std::cout << "================================================" << std::endl;
-
+/*  
     UnDirectedGraph<char, int> GND;
     GND.insertVertex("A", 5);
     GND.insertVertex("B", 50);
@@ -37,5 +82,8 @@ int main(int argc, char *argv[]) {
     bfs<char,int> Y(&GND, "A");
     auto new_graph = Y.get_grafo();
     new_graph->display();
+    */
+
+    Mostrar();
     return EXIT_SUCCESS;
 }
